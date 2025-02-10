4762ceaa8eee3e1d08005d7a59c536cc6e6733e0
Asatbek2024 <qurbonovo2008@gmail.com>
2025-02-10 13:31:38 +0500

    Python float sonlar

---
+++ Data_Types/Python/float.py
@@ -0,0 +1,9 @@
+
+# python dasturlash tilida float sonlar yani butun bo'lmagan sonlar float sonlar hisoblanadi
+
+a = 4.5
+
+print(a)
+print(type(a)) # python dasturlash tilida type() funksiyasi bu data typesni yani qaysi malumot turida ekanligini aytib berdi
+
+``
