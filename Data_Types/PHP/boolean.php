d9ed9775d30a3bd6de00b12917ecf93b21c48319
Asatbek2024 <qurbonovo2008@gmail.com>
2025-02-10 13:19:24 +0500

    boolean data type

---
+++ Data_Types/PHP/boolean.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// php da boolean data type bu true va false ni ifodalaydi yani 
+
+
+$a = 5;
+ 
+
+// agar $a o'zgaruvchisi 5 ga teng bo'lsa true qiymatini chiqar aks holda false yani yolg'on qiymatni chiqar
+
+
+
+if($a == 5)  
+{
+ echo true;
+}else{
+    echo false;
+}
+
+// true bu to'g'ri
+// false bu yolg'on
+    
